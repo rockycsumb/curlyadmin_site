e4daf7ce40440f7b7abb50a55b752a5f57327b4d
rockycsumb <romoreno@csumb.edu>
2020-06-24 21:51:41 +0000

    made server.js file

--- server.js
+++ server.js
@@ -1,38 +1,13 @@
-var sslRedirect = require('heroku-ssl-redirect');
 const express = require('express');
-const connectDB = require('./config/db');
-const cors = require('cors');
-const path = require('path');
 
 const app = express();
 
-// enable ssl redirect
-app.use(sslRedirect());
 
-// Connect Database
-connectDB();
+app.get('/', (req, res)=>res.send('API Running'));
 
-// Init Middleware
-app.use(express.json({extended: false}));
-app.use(cors({origin: 'https://mernstack-shrnureact.run-us-west2.goorm.io'}))
-
-
-// app.get('/', (req, res)=>res.send('API Running'));
-
-// Define Routes
-app.use('/api/users', require('./routes/api/users'));
-app.use('/api/auth', require('./routes/api/auth'));
-app.use('/api/profile', require('./routes/api/profile'));
-app.use('/api/task', require('./routes/api/task'));
-
-//Serve static assets in production
-if(process.env.NODE_ENV === 'production'){
-   //Set Static folder
-	app.use(express.static('client/build'));
-	app.get('*', (req, res)=>{
-		res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
-	})
-}
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, ()=> console.log(`Server started on ${PORT}`));
+
+app.listen(PORT, ()=>console.log(`Server started on port ${PORT}`));
+
+
