fda8b45a798fd8aa2fc9215b167a57a69857d3b0
rockycsumb <romoreno@csumb.edu>
2020-07-01 04:57:09 +0000

    fixed dashbaord task

--- client/src/components/dashboard/DashboardBio.js
+++ client/src/components/dashboard/DashboardBio.js
@@ -23,17 +23,17 @@ const DashboardBio = (props) => {
 										Name: {props.name}
 									</h3>
 									<div class="h5 font-weight-300">
-										Location: <i class="ni location_pin mr-2"></i> {props.location}
+										Location: <i class="ni location_pin mr-2"></i> {!props.location ? <i>Fill in a location</i> : props.location}
 									</div>
 									<div class="h5 mt-4">
-										Company: <i class="ni business_briefcase-24 mr-2"></i>{props.company}
+													Company: <i class="ni business_briefcase-24 mr-2"></i>{!props.company ? <i>Fill in Company Name</i> : props.company}
 									</div>
 									<div>
-										Webste: <i class="ni education_hat mr-2"></i>{props.website}
+										Website: <i class="ni education_hat mr-2"></i>{!props.website ? <i>Fill in a web address</i> : props.website}
 									</div>
 									<hr class="my-4" />
 									A Little about me:
-									<p>{props.bio}</p>
+									<p>{!props.bio ? <i>Say something about yourself</i> : props.bio}</p>
 									
 								</div>
 							</div>
